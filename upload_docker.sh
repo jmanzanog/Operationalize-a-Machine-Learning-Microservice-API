9b87a24c8cfa9b1b93b149094d03fe4ccf3e30d9
josemanzano <j.manzano.g@hotmail.com>
2020-05-10 22:34:00 -0500

    version 1

---
+++ upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=jmanzanog/development
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag 9eb4f1609f10 $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
